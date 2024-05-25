28d514b727329a6d1b9e66d6b7ef942c001a6d8f
Ryan <heryanh@sfu.ca>
2024-05-25 10:42:29 -0700

    first commit

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Ryan</h1>
+
+        <p className="text-gray-500 text-lg lg:text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nulla aliquid at vel! Earum, aut perferendis labore autem corrupti fuga quo in dicta ullam tempore sunt a sequi accusantium quidem.
+          Consectetur aliquid cumque dolores exercitationem! Aut quo doloribus atque! Corporis nisi quisquam, eius quas id fuga aspernatur sit consequatur natus accusantium nobis cumque, assumenda itaque sed a enim error ea!
+        </p>
+    </section>
+  )
+}
+
+export default HeroSection
